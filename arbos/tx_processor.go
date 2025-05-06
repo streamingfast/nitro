22212c2a099215fd7e4885946ab725ca60ba7f8c
Stéphane Duchesneau <stephane.duchesneau@streamingfast.io>
2025-05-06 09:49:13 -0400

    3rd print

--- arbos/tx_processor.go
+++ arbos/tx_processor.go
@@ -663,6 +663,7 @@ func (p *TxProcessor) EndTxHook(gasLeft uint64, success bool) {
 	if p.state.ArbOSVersion() < params.ArbosVersion_2 {
 		posterFeeDestination = p.evm.Context.Coinbase
 	}
+	fmt.Printf("MintBalance, posterFeeDestination: %s, evm state %T\n", posterFeeDestination, p.evm.StateDB)
 	util.MintBalance(&posterFeeDestination, p.PosterFee, p.evm, scenario, tracing.BalanceIncreaseL1PosterFee)
 	if p.state.ArbOSVersion() >= params.ArbosVersion_10 {
 		if _, err := p.state.L1PricingState().AddToL1FeesAvailable(p.PosterFee); err != nil {
